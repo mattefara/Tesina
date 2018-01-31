2d749bf9af4d0388e3cfe1fdb8d345b51be308b4
mattefara <matteofrc@gmail.com>
2018-01-31 22:50:59 +0100

    agguinta punti in tollbar e sidebar migliorata

--- app_per_test/Swipe_sidebar/app/src/main/java/com/example/matte/swipe_sidebar/MainActivity.java
+++ app_per_test/Swipe_sidebar/app/src/main/java/com/example/matte/swipe_sidebar/MainActivity.java
@@ -1,29 +1,38 @@
 package com.example.matte.swipe_sidebar;
 
-import android.app.Activity;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     List<SidebarAction> actions;
     private DrawerLayout drawerLayout;
     private RecyclerView listView;
-    final private String[] ACTION_MESSAGES = new String[] {"Home", "Info", "Utente", "Sedi"};
-    final private int[] ICONS = new int[]{R.drawable.ic_home,R.drawable.ic_info,R.drawable.ic_account_circle,R.drawable.ic_place};
+    final private String[] ACTION_MESSAGES = new String[] {"Home", "Info", "Utente", "Sedi", "Impostazioni"};
+    final private int[] ICONS = new int[]{R.drawable.ic_home,R.drawable.ic_info,R.drawable.ic_account_circle,R.drawable.ic_place,R.drawable.ic_settings};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //products = getResources().getStringArray(R.array.lista_elementi);
-        //drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
         listView = findViewById(R.id.list);
         listView.setHasFixedSize(true);
         listView.setLayoutManager(new LinearLayoutManager(this));
@@ -38,4 +47,29 @@ public class MainActivity extends Activity {
 
         listView.setAdapter(lista);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(getApplicationContext(),"x",Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.action_setting:
+                Toast.makeText(getApplicationContext(),"x",Toast.LENGTH_SHORT).show();
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
