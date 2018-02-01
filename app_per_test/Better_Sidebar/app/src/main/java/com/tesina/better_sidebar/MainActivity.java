e1e83f15608e2a1d1e4b12d2a81961277d0830f2
mattefara <matteofrc@gmail.com>
2018-02-01 14:57:24 +0100

    sidebar finita

--- app_per_test/Better_Sidebar/app/src/main/java/com/tesina/better_sidebar/MainActivity.java
+++ app_per_test/Better_Sidebar/app/src/main/java/com/tesina/better_sidebar/MainActivity.java
@@ -1,27 +1,40 @@
 package com.tesina.better_sidebar;
 
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.internal.NavigationMenu;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     NavigationView sideBar;
     DrawerLayout drawerLayout;
+    LinearLayout sidebarLayout;
     android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sidebarLayout = findViewById(R.id.sidebarLayout);
+        int height = getStatusBarHeight();
+        sidebarLayout.setPadding(0,height,0,0);
+        setStatusBarTransparent();
 
-        //toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
         sideBar = findViewById(R.id.sidebar);
         if (sideBar != null){ sideBar.setNavigationItemSelectedListener(this); }
@@ -56,5 +69,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         drawerLayout.closeDrawers();
         return true;
+    }
+
+    private void setStatusBarTransparent(){
+        if (Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            w.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    private int getStatusBarHeight(){
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        Log.i("Status bar height",statusBarHeight + "");
+        return statusBarHeight;
     }
 }
