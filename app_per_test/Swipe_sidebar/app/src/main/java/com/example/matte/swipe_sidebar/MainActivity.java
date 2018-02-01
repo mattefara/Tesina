0bce6419b86fc5efc31b1b6527e5ddad9d1cff38
mattefara <matteofrc@gmail.com>
2018-02-01 08:14:26 +0100

    modifiche introslider

--- app_per_test/Swipe_sidebar/app/src/main/java/com/example/matte/swipe_sidebar/MainActivity.java
+++ app_per_test/Swipe_sidebar/app/src/main/java/com/example/matte/swipe_sidebar/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.matte.swipe_sidebar;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +26,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setStatusBarTransparent();
 
         //products = getResources().getStringArray(R.array.lista_elementi);
         //drawerLayout = findViewById(R.id.drawer_layout);
@@ -37,5 +43,14 @@ public class MainActivity extends Activity {
         ListAdapter lista = new ListAdapter(actions,getApplicationContext());
 
         listView.setAdapter(lista);
+    }
+
+    private void setStatusBarTransparent(){
+        if (Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            w.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
