2400a732ec73ee91542d490a12a9a4209005fffd
Matteo Faraci <matteofrc@gmail.com>
2018-05-27 13:00:23 +0200

    New function Like added to Firebae functions

--- smop_firebase_functions/functions/index.js
+++ smop_firebase_functions/functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+  const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
@@ -7,5 +7,30 @@ admin.initializeApp(functions.config().firebase);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
+  console.log("La funzione hello world è partita")
   response.send("Hello from Firebase!");
 });
+
+exports.like = functions.https.onCall((data, context) => {
+  const str = data.text;
+  console.log(str)
+  const example = admin.database().ref("query2").orderByChild("name")
+  var products = []
+  const x =  example.once('value').then( function (snapshot) {
+      snapshot.forEach(function(childSnapShot){
+          const resChild = childSnapShot.val()
+          const name = resChild.name
+          
+          if (name.substring(0, str.length).toUpperCase() === str.toUpperCase()){
+              products.push({
+                  barcode : resChild.barcode,
+                  name : name,
+                  quantity :resChild.quantity
+              })
+          }
+      })
+      return products;
+  })
+  console.log("xxxxx",x)
+  return x;
+})
